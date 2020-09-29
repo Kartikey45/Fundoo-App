30de8cc49be1d853441a39704b2cddb2da9c8b2e
Kartikey45 <singhkartikey45@gmail.com>
2020-09-29 14:49:20 +0530

    REFACTORED : Registration Page

--- fundoo-app/src/Components/Registration/Register.jsx
+++ fundoo-app/src/Components/Registration/Register.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import "./Register.scss";
 import Button from "react-bootstrap/Button";
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+
 
 export default class Register extends React.Component {
   constructor() {
@@ -13,13 +14,71 @@ export default class Register extends React.Component {
     return (
       <div className="Manipulation">
         <div className="RegistrationBody">
-          <div>
-            <div className="jhdf">
-              <h3 className="FontSerif">Fundoo</h3>
-              <div className = "hfgt">
-              <br></br><br></br><TextField id="outlined-basic" label="First Name" variant="outlined" />
-              </div>
+          <div className="inputPanelBody">
+            <div className="fundooTextAlign">
+              <div className="Fcolor">F</div>
+              <div className="Fcolor">u</div>
+              <div className="Fcolor">n</div>
+              <div className="Fcolor">d</div>
+              <div className="Fcolor">o</div>
+              <div className="Fcolor">o</div>
             </div>
+            <div className="font">
+              <h5>Create your Fundoo Account</h5>
+            </div>
+
+            <div className="InputPanel">
+              <TextField className = "textSize"
+                id="outlined-basic"
+                label="First Name"
+                variant="outlined"
+                size="small"
+              />
+              <TextField className = "textSize"
+                id="outlined-basic"
+                label="Last Name"
+                variant="outlined"
+                size="small"
+              />
+            </div>
+            <div className="InputPanel1">
+              <TextField
+                id="outlined-basic"
+                label="E mail"
+                variant="outlined"
+                fullWidth
+                size="small"
+              />
+            </div>
+            <div className="flex">
+              <h7>You can use letters , numbers and periods</h7>
+            </div>
+            <div className="Passwordfield">
+              <TextField className = "textSize"
+                id="outlined-basic"
+                label="Password"
+                variant="outlined"
+                size="small"
+              />
+              <TextField className = "textSize"
+                id="outlined-basic"
+                label="Confirm Password"
+                variant="outlined"
+                size="small"
+              />
+            </div>
+            <div className="flex">
+              <h7>
+                Use 8 or more characters mix with letters, numbers & symbols
+              </h7>
+            </div>
+            <div className="buttons">
+              <Button variant="primary">Sign Up</Button>
+              <Button variant="primary">Login</Button>
+            </div>
+          </div>
+          <div className = "image">
+          <img src="https://ssl.gstatic.com/accounts/signup/glif/account.svg" alt="" width="244" height="244"></img>
           </div>
         </div>
       </div>
